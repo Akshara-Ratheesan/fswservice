e0f2bf883822b24fb67155b12cf28d900adbf4fd
Akshara-Ratheesan <117821985+Akshara-Ratheesan@users.noreply.github.com>
2022-11-10 12:55:11 +0530

    Update index.js
    second commit

--- index.js
+++ index.js
@@ -2,23 +2,36 @@ var express = require("express"); //requiring express module
 var app = express(); //creating express instance
 var querystring = require('querystring');
 const axios = require('axios');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 var data = querystring.stringify({
-grant_type: "client_credentials",    
-client_id: "watson-orchestrate",    
-client_secret:"ca81109d-312d-4ed3-9cf0-19398e26ea9d" });
-app.get('/screeningAI',async (req,res) => {
-try { 
-const jwt_token= await axios.post('https://keycloak-edu-keycloak.apps.openshift-01.knowis.cloud/auth/realms/education/protocol/openid-connect/token',data,{     
-headers: {      
-'Content-Type': 'application/x-www-form-urlencoded',   
-'Content-Length': Buffer.byteLength(data)  
-}    }    );    const axiosInstance = axios.create({  
-headers: {          
-'Authorization': 'Bearer '+jwt_token.data.access_token   
-}    });    
-const response = await axiosInstance.get('https://education-dev.apps.openshift-01.knowis.cloud/candhun/api/huncan/getChannel');  
-res.send(response.data)} catch (error) {    console.log(error);}
-        });
+        grant_type: "client_credentials",    
+        client_id: "watson-orchestrate",    
+        client_secret:"ca81109d-312d-4ed3-9cf0-19398e26ea9d"
+ });
+app.get('/getChannel',async (req,res) => {
+        try { 
+                const jwt_token= await axios.post('https://keycloak-edu-keycloak.apps.openshift-01.knowis.cloud/auth/realms/education/protocol/openid-connect/token',data,{     
+                        headers: {      
+                                'Content-Type': 'application/x-www-form-urlencoded',   
+                                'Content-Length': Buffer.byteLength(data)  
+                        }    
+                }    );    
+                let jobProf = req.query.jobProfile;
+                let config = {
+                        headers: { 'Authorization': 'Bearer ' + jwt_token.data.access_token },
+                        params: {
+                            jobProfile: jobProf
+                        }
+                    }
+                const axiosInstance = axios.create({  
+                        headers: {          
+                        'Authorization': 'Bearer '+jwt_token.data.access_token   
+                        }   
+                 });    
+                const response = await axiosInstance.get('https://education-dev.apps.openshift-01.knowis.cloud/candhun/api/huncan/getChannel',config);  
+                res.send(response.data)
+        } 
+        catch (error) {    console.log(error);}
+});
         
 app.listen(PORT, function() { console.log("Node server is running..");});
